67d8ae89a4dc2be4b887a250406ec2673e5eda20
bcline01 <brookecline.bc@gmail.com>
2024-09-16 18:18:35 -0400

    debug my split

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -8,7 +8,7 @@ interface JwtPayload {
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
   if (authHeader) {
-    const token = authHeader.split('')[1];
+    const token = authHeader.split(' ')[1];
     const secretKey = process.env.JWT_SECRET_KEY || '';
     jwt.verify(token, secretKey, (err, user) => {
       if (err) {
